f74b7d4cba8c73a589ee78ffa2cf439e1ac4be41
Rohit Agrawal <rohit.agrawal@databricks.com>
2022-07-09 01:37:39 -0700

    Added LUA Support For Listener StreamInfo
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- source/extensions/filters/http/lua/wrappers.cc
+++ source/extensions/filters/http/lua/wrappers.cc
@@ -170,6 +170,16 @@ int StreamInfoWrapper::luaDynamicMetadata(lua_State* state) {
   return 1;
 }
 
+int ListenerStreamInfoWrapper::luaListenerDynamicMetadata(lua_State* state) {
+  if (listener_dynamic_metadata_wrapper_.get() != nullptr) {
+    listener_dynamic_metadata_wrapper_.pushStack();
+  } else {
+    listener_dynamic_metadata_wrapper_.reset(
+        ListenerDynamicMetadataMapWrapper::create(state, *this), true);
+  }
+  return 1;
+}
+
 int StreamInfoWrapper::luaDownstreamSslConnection(lua_State* state) {
   const auto& ssl = stream_info_.downstreamAddressProvider().sslConnection();
   if (ssl != nullptr) {
@@ -216,7 +226,14 @@ int StreamInfoWrapper::luaRequestedServerName(lua_State* state) {
 DynamicMetadataMapIterator::DynamicMetadataMapIterator(DynamicMetadataMapWrapper& parent)
     : parent_{parent}, current_{parent_.streamInfo().dynamicMetadata().filter_metadata().begin()} {}
 
+ListenerDynamicMetadataMapIterator::ListenerDynamicMetadataMapIterator(
+    ListenerDynamicMetadataMapWrapper& parent)
+    : parent_{parent}, current_{parent_.streamInfo().dynamicMetadata().filter_metadata().begin()} {}
+
 StreamInfo::StreamInfo& DynamicMetadataMapWrapper::streamInfo() { return parent_.stream_info_; }
+const StreamInfo::StreamInfo& ListenerDynamicMetadataMapWrapper::streamInfo() {
+  return parent_.listener_stream_info_;
+}
 
 int DynamicMetadataMapIterator::luaPairsIterator(lua_State* state) {
   if (current_ == parent_.streamInfo().dynamicMetadata().filter_metadata().end()) {
@@ -225,6 +242,19 @@ int DynamicMetadataMapIterator::luaPairsIterator(lua_State* state) {
   }
 
   lua_pushlstring(state, current_->first.data(), current_->first.size());
+  Filters::Common::Lua::MetadataMapHelper::createTable(state, current_->second.fields());
+
+  current_++;
+  return 2;
+}
+
+int ListenerDynamicMetadataMapIterator::luaListenerDynamicMetadataPairsIterator(lua_State* state) {
+  if (current_ == parent_.streamInfo().dynamicMetadata().filter_metadata().end()) {
+    parent_.iterator_.reset();
+    return 0;
+  }
+
+  lua_pushstring(state, current_->first.c_str());
   Filters::Common::Lua::MetadataMapHelper::createTable(state, current_->second.fields());
 
   current_++;
@@ -271,6 +301,29 @@ int DynamicMetadataMapWrapper::luaPairs(lua_State* state) {
 
   iterator_.reset(DynamicMetadataMapIterator::create(state, *this), true);
   lua_pushcclosure(state, DynamicMetadataMapIterator::static_luaPairsIterator, 1);
+  return 1;
+}
+
+int ListenerDynamicMetadataMapWrapper::luaListenerDynamicMetadataGet(lua_State* state) {
+  const char* filter_name = luaL_checkstring(state, 2);
+  const auto& metadata = streamInfo().dynamicMetadata().filter_metadata();
+  const auto filter_it = metadata.find(filter_name);
+  if (filter_it == metadata.end()) {
+    return 0;
+  }
+
+  Filters::Common::Lua::MetadataMapHelper::createTable(state, filter_it->second.fields());
+  return 1;
+}
+
+int ListenerDynamicMetadataMapWrapper::luaListenerDynamicMetadataPairs(lua_State* state) {
+  if (iterator_.get() != nullptr) {
+    luaL_error(state, "cannot create a second iterator before completing the first");
+  }
+
+  iterator_.reset(ListenerDynamicMetadataMapIterator::create(state, *this), true);
+  lua_pushcclosure(
+      state, ListenerDynamicMetadataMapIterator::static_luaListenerDynamicMetadataPairsIterator, 1);
   return 1;
 }
 
