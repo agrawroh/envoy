3fe65a966069ad45245c5d5744e0b62328e98c66
Rohit Agrawal <rohit.agrawal@databricks.com>
2024-01-06 21:41:01 -0800

    Disable Hyperscan Contrib Ext
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- contrib/mysql_proxy/filters/listener/source/config.cc
+++ contrib/mysql_proxy/filters/listener/source/config.cc
@@ -1,5 +1,5 @@
-#include "envoy/extensions/filters/listener/http_inspector/v3/http_inspector.pb.h"
-#include "envoy/extensions/filters/listener/http_inspector/v3/http_inspector.pb.validate.h"
+#include "envoy/extensions/filters/listener/mysql_inspector/v3/mysql_inspector.pb.h"
+#include "envoy/extensions/filters/listener/mysql_inspector/v3/mysql_inspector.pb.validate.h"
 #include "envoy/registry/registry.h"
 #include "envoy/server/filter_config.h"
 
@@ -8,41 +8,38 @@
 namespace Envoy {
 namespace Extensions {
 namespace ListenerFilters {
-namespace HttpInspector {
+namespace MySQLInspector {
 
 /**
- * Config registration for the Http inspector filter. @see NamedNetworkFilterConfigFactory.
+ * Config registration for the MySQL inspector filter. @see NamedNetworkFilterConfigFactory.
  */
-class HttpInspectorConfigFactory : public Server::Configuration::NamedListenerFilterConfigFactory {
+class MySqlInspectorConfigFactory : public Server::Configuration::NamedListenerFilterConfigFactory {
 public:
   // NamedListenerFilterConfigFactory
   Network::ListenerFilterFactoryCb createListenerFilterFactoryFromProto(
       const Protobuf::Message&,
       const Network::ListenerFilterMatcherSharedPtr& listener_filter_matcher,
       Server::Configuration::ListenerFactoryContext& context) override {
-    MySQLInspector::ConfigSharedPtr config(std::make_shared<Config>(context.scope()));
+    MySQLInspector::ConfigSharedPtr config(std::make_shared<MySQLInspector::Config>(context.scope()));
     return
         [listener_filter_matcher, config](Network::ListenerFilterManager& filter_manager) -> void {
-          filter_manager.addAcceptFilter(listener_filter_matcher, std::make_unique<Filter>(config));
+          filter_manager.addAcceptFilter(listener_filter_matcher, std::make_unique<MySQLInspector::Filter>(config));
         };
   }
 
   ProtobufTypes::MessagePtr createEmptyConfigProto() override {
-    return std::make_unique<
-        envoy::extensions::filters::listener::http_inspector::v3::HttpInspector>();
+    return std::make_unique<envoy::extensions::filters::listener::mysql_inspector::v3::MySQLInspector>();
   }
 
-  std::string name() const override { return "envoy.filters.listener.http_inspector"; }
+  std::string name() const override { return "envoy.filters.listener.mysql_inspector"; }
 };
 
 /**
- * Static registration for the http inspector filter. @see RegisterFactory.
+ * Static registration for the MySQL inspector filter. @see RegisterFactory.
  */
-REGISTER_FACTORY(HttpInspectorConfigFactory,
-                 Server::Configuration::NamedListenerFilterConfigFactory){
-    "envoy.listener.http_inspector"};
+REGISTER_FACTORY(MySqlInspectorConfigFactory, Server::Configuration::NamedListenerFilterConfigFactory){"envoy.listener.mysql_inspector"};
 
-} // namespace HttpInspector
+} // namespace MySQLInspector
 } // namespace ListenerFilters
 } // namespace Extensions
 } // namespace Envoy
