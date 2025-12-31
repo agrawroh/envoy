f90ac1150d91ee3d00eb73dab24bd2e27ca08e80
Rohit Agrawal <rohit.agrawal@databricks.com>
2025-12-31 15:11:33 +0530

    dynamic_modules: add more methods to listener filters ABI
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "ecb768e12042cb69b0f1ad5446ffe76085009961157ab4c8c9277beec739572e";
+const char* kAbiVersion = "acfcc54ed2d81642aba5a3db5f22fe63b162d710d0f2dee16e29990736ff74fa";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
